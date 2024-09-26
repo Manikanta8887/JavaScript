b31e837a42e4b6e81c55a4ae178939bcbad0b420
Manikanta8887 <swamimanikanta66@gmail.com>
2024-09-26 21:58:46 +0530

    Adding Files

---
+++ js.js
@@ -0,0 +1,98 @@
+
+// Primitive DataTypes
+
+var name="Manikanta";
+var age=22;
+var degree="B.Tech";
+var hobbies=undefined;
+var exercise=null;
+
+console.log(name);
+console.log(age);
+console.log(degree);
+console.log(true);
+console.log(false);
+
+let a=10;
+console.log(a);
+const b=20;
+console.log(b);
+
+
+var x=20;
+var y=10;
+
+if(x==y){
+    let x=30;
+    console.log(x);
+    const z=40;
+    console.log(z);
+    console.log(true);
+}
+console.log(x);
+
+// Non-Primitive DataTypes
+
+var Mani={
+    name:"Manikanta",
+    age:22,
+    job:"Software Developer",
+    salary:800000,
+    p:{
+        game:"cricket",
+        onlinegame:"freefire"
+    }
+}
+
+console.log(Mani);
+
+Mani.name="Manikanta Thirnati";
+Mani.loc="Hyderabad";
+console.log(Mani);
+
+
+var manikanta=["Mani", "BTech", "age", "job", 22];
+console.log(manikanta);
+manikanta[5]="Salary";
+console.log(manikanta);
+
+manikanta.Mani=[
+    name="Manikanta",
+    age=22,
+    job="Software Developer",
+    salary=800000,
+    p={
+        game:"cricket",
+        onlinegame:"freefire"
+    }
+]
+
+manikanta.thirnati={
+    name:"Manikanta",
+    age:22,
+    job:"Software Developer",
+    salary:800000,
+    p:{
+        game:"cricket",
+        onlinegame:"freefire"
+    }
+}
+
+
+console.log(manikanta);
+
+manikanta.thirnati.name="Manikanta Thirnati";
+manikanta.thirnati.loc="Hyderabad";
+
+console.log(manikanta);
+
+
+
+
+
+
+// we can also declare an array to assign values after
+var pawankalyan=[];
+console.log(pawankalyan);
+
+
